b532d4118ffd0628d8b8ad058303ed5235350520
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-07-14 02:07:14 +0300

    paddings

--- components/Top.tsx
+++ components/Top.tsx
@@ -15,7 +15,6 @@ const Top: FC<TopProps> = ({ title }) => {
             title
           ) : (
             <>
-              {/* <span style={{ color: '#a91c1c' }}>Counter-Strike 1.6</span> Download */}
               <span style={{ color: '#a91c1c' }}>Counter-Strike 1.6</span> Download
             </>
           )}
@@ -25,13 +24,6 @@ const Top: FC<TopProps> = ({ title }) => {
         and is protected against malicious server commands
         <br /> <span className='text-amber-300'>CS</span> is comapatible with the latest PC Windows
         XP, Windows 7, Windows 8, Windows 10 versions.
-        {/* Active servers - <span className='text-amber-300 inline-block min-w-[1rem]'>{servers}</span>{' '}
-        <br />
-        Filled with <span className='text-amber-300 inline-block min-w-[2rem]'>{players}</span>{' '}
-        players out of
-        <span className='text-amber-300 ml-1 inline-block min-w-[2rem]'> {maxPlayers}</span> total
-        slots. Percentage player count -{' '}
-        <span className='text-amber-300 inline-block min-w-[1rem]'>{percentage}%</span> */}
       </div>
       <Image
         className='ml-auto pt-5 sm:pt-0 opacity-30 sm:opacity-100'
@@ -49,7 +41,7 @@ const Top: FC<TopProps> = ({ title }) => {
         alt='CS 1.6 Download'
         title='Counter-Strike 1.6'
       />
-      <DownloadButtons className='mt-32' />
+      <DownloadButtons className='mt-6 sm:mt-32' />
     </div>
   );
 };
